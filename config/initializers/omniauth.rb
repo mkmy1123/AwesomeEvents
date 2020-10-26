9c3298b8e7c0f26dcc26b43624a8f3e7888d75e9
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-26 15:36:44 +0900

    for github actions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github,
-    Rails.application.credentials.github[:client_id],
-    Rails.application.credentials.github[:client_secret]
+  if Rails.env.development? || Rails.env.test?
+    provider :github, "d10a5036dad139a30dd5", "09b7355d2062d76d001373b834e364815df97539"
+  else
+    provider :github,
+      Rails.application.credentials.github[:client_id],
+      Rails.application.credentials.github[:client_secret]
+  end
 end
