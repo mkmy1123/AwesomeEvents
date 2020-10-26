b99cd19a18965d4dfb26dc98775c15bcfdca777d
Maki Miyagawa <mkmy1123@gmail.com>
2020-10-26 16:07:31 +0900

    fix for pass CI

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resource :retirements, only: %i[new create]
 
-  resources :events, only: %i[new create show edit update destory] do
+  resources :events, only: %i[new create show edit update destroy] do
     resources :tickets, only: %i[new create destroy]
   end
 
